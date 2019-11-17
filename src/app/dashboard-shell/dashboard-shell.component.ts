057f5cb45048728c09477dbabee0b4208035c6fa
Mysteriedingens <josefsle97@gmail.com>
2019-11-17 18:09:29 +0100

    single Cocktail view and functionality

--- src/app/dashboard-shell/dashboard-shell.component.ts
+++ src/app/dashboard-shell/dashboard-shell.component.ts
@@ -1,27 +1,25 @@
-import { alcoholicDrink } from './../models/alcoholicDrink.model';
-import { Component, OnInit } from '@angular/core';
+import { alcoholicDrink } from "./../models/alcoholicDrink.model";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-dashboard-shell',
-  templateUrl: './dashboard-shell.component.html',
-  styleUrls: ['./dashboard-shell.component.scss']
+  selector: "app-dashboard-shell",
+  templateUrl: "./dashboard-shell.component.html",
+  styleUrls: ["./dashboard-shell.component.scss"]
 })
 export class DashboardShellComponent implements OnInit {
   firstWelcome: boolean = true;
   singleCocktail: alcoholicDrink;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  private clickedFirstWelcome(){
+  private clickedFirstWelcome() {
     console.log("click");
     this.firstWelcome = false;
   }
 
-  getAlcoholicCocktail(id: String){
-    console.log("clicked" + id );
+  getAlcoholicCocktail(id: String) {
+    console.log("clicked" + id);
     /* this.cocktailSingle = this.cocktailSvc.getAlcoholicCocktail(id); */
   }
-
 }
--- src/app/services/cocktail.service.ts
+++ src/app/services/cocktail.service.ts
@@ -1,43 +1,32 @@
-import { Drink } from './../models/Drink.model';
-import { alcoholicDrink } from './../models/alcoholicDrink.model';
-import { Injectable, OnChanges } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, Subject, BehaviorSubject, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { alcoholicDrink } from "./../models/alcoholicDrink.model";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, Subject } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CocktailService {
-
   cocktailSubject = new Subject<alcoholicDrink>();
 
   constructor(private http: HttpClient) {}
 
   readonly endpoint: String = "https://www.thecocktaildb.com/api/json/v1/1/";
 
-
-    getAlcoholicCocktails():  Observable<alcoholicDrink[]>{
-    return this.http.get(this.endpoint+"filter.php?c=Cocktail").pipe(
-      map(
-        (data: any) => 
-          data.drinks.map(
-          alcoholicDrink.adapt
-        )
-      )
-    )
+  getAlcoholicCocktails(): Observable<alcoholicDrink[]> {
+    return this.http
+      .get(this.endpoint + "filter.php?c=Cocktail")
+      .pipe(map((data: any) => data.drinks.map(alcoholicDrink.adapt)));
   }
 
-    getAlcoholicCocktail(id: String){
-      this.http.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007").pipe(
-        map(
-          (data: any) => data.drinks.map(
-            alcoholicDrink.adapt
-          )
-        )
-      ).subscribe(
-        res => { this.cocktailSubject.next(res) }
-      )
-    }
-    
+  getAlcoholicCocktail(id: String) {
+    this.http
+      .get(this.endpoint + "lookup.php?i=" + id)
+      .pipe(map((data: any) => data.drinks.map(alcoholicDrink.adapt)))
+      .subscribe(res => {
+        console.log(res);
+        this.cocktailSubject.next(res[0]);
+      });
   }
+}
